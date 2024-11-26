5769806065212cf0e3aed45f72e5d42f8797deaa
Santiago Negreira <sansfleitas@gmail.com>
2024-11-26 20:23:25 -0300

    Create OrderController

---
+++ src/main/java/com/sansarch/bookstore_order_service/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.sansarch.bookstore_order_service.controller;
+
+import com.sansarch.bookstore_order_service.dto.PlaceOrderInputDto;
+import com.sansarch.bookstore_order_service.dto.PlaceOrderOutputDto;
+import com.sansarch.bookstore_order_service.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<PlaceOrderOutputDto> placeOrder(@RequestBody PlaceOrderInputDto input) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(orderService.placeOrder(input));
+    }
+
+    @GetMapping("/{id}")
+    public void getOrder(@PathVariable Long id) {
+
+    }
+
+    @GetMapping("/users/{id}/orders")
+    public void getAllOrdersFromUser(@PathVariable Long id) {
+
+    }
+}
