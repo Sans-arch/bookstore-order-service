57648d956d20c34b02c396cb438eb979382dd568
Santiago Negreira <sansfleitas@gmail.com>
2025-01-17 14:46:23 -0300

    Add new polymorphic method for publish to publish messages specifying the routingKey

--- src/main/java/com/sansarch/bookstore_order_service/domain/common/messaging/MessagePublisher.java
+++ src/main/java/com/sansarch/bookstore_order_service/domain/common/messaging/MessagePublisher.java
@@ -1,8 +1,7 @@
 package com.sansarch.bookstore_order_service.domain.common.messaging;
 
 
-import com.sansarch.bookstore_order_service.domain.order.event.Event;
-
 public interface MessagePublisher {
     void publish(Event event, String exchange);
+    void publish(Event event, String exchange, String routingKey);
 }
