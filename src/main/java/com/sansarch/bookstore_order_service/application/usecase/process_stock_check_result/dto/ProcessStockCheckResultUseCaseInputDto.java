0bcad87c46484636b3b8a6868c7bb353104d37ae
Santiago Negreira <sansfleitas@gmail.com>
2025-01-17 14:43:41 -0300

    Create ProcessStockCheckResultUseCase

---
+++ src/main/java/com/sansarch/bookstore_order_service/application/usecase/process_stock_check_result/dto/ProcessStockCheckResultUseCaseInputDto.java
@@ -0,0 +1,7 @@
+package com.sansarch.bookstore_order_service.application.usecase.process_stock_check_result.dto;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ProcessStockCheckResultUseCaseInputDto {
+}
---
+++ src/main/java/com/sansarch/bookstore_order_service/application/usecase/process_stock_check_result/dto/ProcessStockCheckResultUseCaseOutputDto.java
@@ -0,0 +1,9 @@
+package com.sansarch.bookstore_order_service.application.usecase.process_stock_check_result.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class ProcessStockCheckResultUseCaseOutputDto {
+}
